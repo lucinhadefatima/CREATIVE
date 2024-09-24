6743a0c131f4551b0aabd991028a6bd3f6350ece
lucinhadefatima <lucweb010@gmail.com>
2024-09-24 20:31:26 -0300

    versao 1.0

---
+++ app.py
@@ -0,0 +1,21 @@
+#entrada de dados
+nome = input("imforme seu nome:")
+cpf = input("imforme seu cpf:")
+telefone = input("imforme seu telefone:")
+email = input("imforme seu email:")
+endereco = input("imforme seu endereco:")
+genero = input("imforme seu genero:")
+escolaridade = input("imforme seu escolaride:")
+signo = input("imforme seu signo:")
+tipo_sanguineo = input("imforme seu tipo sanguineo:")
+
+#saida de dados
+print(f"nome:{nome}.")
+print(f"cpf:{cpf}.")
+print(f"telefone:{telefone}.")
+print(f"email:{email}.")
+print(f"endereco:{endereco}.")
+print(f"genero:{genero}.")
+print(f"escolaridade:{escolaridade}.")
+print(f"signo:{signo}.")
+print(f"tipo sanguinio:{tipo sanguineo}.")
